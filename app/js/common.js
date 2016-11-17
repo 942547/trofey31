287647b611ce327979e64d1470bdd7800b63c00e
942547 <942547@gmail.com>
2016-11-17 08:31:04 +0300

    add codepen unslider

--- app/js/common.js
+++ app/js/common.js
@@ -31,9 +31,15 @@ api.bind("closed", function () {
 });
 	
 	// Unslider
-	$('.my-slider').unslider({
-		autoplay: true,
-		arrows: false
-	});
+	$('.banner').unslider({
+      speed: 500,               //  The speed to animate each slide (in milliseconds)
+      delay: 5000,              //  The delay between slide animations (in milliseconds)
+      complete: function() {},  //  A function that gets called after every slide animation
+      keys: false,               //  Enable keyboard (left, right) arrow shortcuts
+      dots: false,               //  Display dot navigation
+      fluid: true,             //  Support responsive design. May break non-responsive designs
+      autoplay: true,
+      arrows: false
+    });
 
 });
