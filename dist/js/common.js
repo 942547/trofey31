759850a2a29f5a1013b33ca0e620d70670f9cbe2
942547 <942547@gmail.com>
2016-11-19 12:16:40 +0300

    add dist

---
+++ dist/js/common.js
@@ -0,0 +1,64 @@
+$(function() {
+
+	$(".sf-menu").superfish({
+		delay: 200,
+		speed: "fast",
+		cssArrows: false
+	})
+	.after("<div id='mobile-menu'>").clone().appendTo("#mobile-menu");
+	$("#mobile-menu").find("*").attr("style", "");
+	$("#mobile-menu").children("ul").removeClass("sf-menu").removeClass("sf-menu_theme_banket")
+	.parent().mmenu({
+		extensions : [ 'widescreen', 'theme-white', 'effect-menu-slide', 'pagedim-black' ],
+		navbar: {
+			title: "Меню"
+		}
+	});
+
+	$("#mobile-menu").mmenu({
+		extensions      : [ 'widescreen', 'theme-white', 'effect-menu-slide', 'pagedim-black' ],
+		navbar: {
+			title: "Меню"
+		}
+	});
+
+	$(".toggle-mnu").click(function() {
+		$(this).toggleClass("on");
+	});
+	var api = $("#mobile-menu").data("mmenu");
+	api.bind("closed", function () {
+		$(".toggle-mnu").removeClass("on");
+	});
+
+
+	//initialize swiper when document ready  
+	var mySwiper = new Swiper ('.swiper-container', {
+		pagination: '.swiper-pagination',
+		nextButton: '.swiper-button-next',
+		prevButton: '.swiper-button-prev',
+		paginationClickable: true,
+		spaceBetween: 50,
+		centeredSlides: true,
+		autoplay: 7000,
+		loop: true,
+		autoplayDisableOnInteraction: false
+	});
+
+	// popup
+	$('.popup-with-move-anim').magnificPopup({
+		type: 'inline',
+
+		fixedContentPos: false,
+		fixedBgPos: true,
+
+		overflowY: 'auto',
+
+		closeBtnInside: true,
+		preloader: false,
+		
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-slide-bottom'
+	});
+
+});
