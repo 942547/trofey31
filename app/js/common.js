cf4c0c39f0b4daa2d7fe45862745dca203c7b9ae
942547 <942547@gmail.com>
2016-12-03 04:13:10 +0300

    debug

--- app/js/common.js
+++ app/js/common.js
@@ -7,7 +7,7 @@ $(function() {
 	})
 	.after("<div id='mobile-menu'>").clone().appendTo("#mobile-menu");
 	$("#mobile-menu").find("*").attr("style", "");
-	$("#mobile-menu").children("ul").removeClass("sf-menu").removeClass("sf-menu_theme_banket")
+	$("#mobile-menu").children("menu").removeClass("sf-menu").removeClass("sf-menu_theme_banket")
 	.parent().mmenu({
 		extensions : [ 'widescreen', 'theme-white', 'effect-menu-slide', 'pagedim-black' ],
 		navbar: {
